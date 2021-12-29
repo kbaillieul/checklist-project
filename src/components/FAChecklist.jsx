eba02867447b75e6027c45f4f73b8543f6ef209a
Kristine Baillieul <kbaillieul1@gmail.com>
2021-12-28 21:04:52 -0330

    Form elements added to FA checklist

--- src/components/FAChecklist.jsx
+++ src/components/FAChecklist.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const FAChecklist = () => {
-  return <div></div>;
+  return (
+    <div>
+      <form className="form">
+        <div className="form-control">
+          <label>Harness ID Number</label>
+          <input type="text"></input>
+        </div>
+        <div className="form-control">
+          <label>Webbing</label>
+          <input type="radio" value="OK" name="OK"></input>
+          <input type="radio" value="Issue" name="Issue"></input>
+          <input type="radio" value="NA" name="NA"></input>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default FAChecklist;
