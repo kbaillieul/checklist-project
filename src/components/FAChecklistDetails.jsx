492612a5ddf50902e45e8be2a8448f691d0d7c97
Kristine Baillieul <kbaillieul1@gmail.com>
2022-02-04 16:57:28 -0330

    Formatting completed checklists details

--- src/components/FAChecklistDetails.jsx
+++ src/components/FAChecklistDetails.jsx
@@ -6,15 +6,34 @@ const FAChecklistDetails = () => {
   let params = useParams();
   let checklist = getFAFromLocal();
   let selectedChecklist = [];
+  let FADate;
   for (let i = 0; i < checklist.length; i++) {
     if (checklist[i].key === params.key) {
       selectedChecklist = checklist[i];
+      FADate = new Date(checklist[i].date);
+      console.log(checklist[i]);
     }
   }
   return (
     <div>
-      <h2> Date Completed: {selectedChecklist.date}</h2>
-      <h2> Completed by: {selectedChecklist.employeeName}</h2>
+      <h4> 1. Date Completed: {FADate.toString()}</h4>
+      <h4> 2. Completed by: {selectedChecklist.employeeName}</h4>
+      <h4> 3. Location: {selectedChecklist.location}</h4>
+      <h4> 4. Task: {selectedChecklist.task}</h4>
+      <h4>
+        {" "}
+        5. There is an applicable and realistic rescue plan in place for this
+        work: {selectedChecklist.plan === true ? "True" : "False"}
+      </h4>
+      <h4>
+        {" "}
+        6. I am not working alone. Working alone is never permitted in a
+        scenario requiring fall arrest:{" "}
+        {selectedChecklist.alone === true ? "True" : "False"}
+      </h4>
+      <h4> 7. Harness ID Number: {selectedChecklist.idNumber}</h4>
+      <h4> 8. Webbing: {selectedChecklist.webbing}</h4>
+      <h4> 9. D Rings: {selectedChecklist.dRing}</h4>
     </div>
   );
 };
