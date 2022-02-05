567b6f5cf41deedf61ee290cc7407f76864ed49c
Kristine Baillieul <kbaillieul1@gmail.com>
2022-02-05 12:46:41 -0330

    Added buttons, formatting

--- src/components/FAMain.jsx
+++ src/components/FAMain.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useNavigate, Outlet } from "react-router-dom";
 import CompletedFAChecklistsMap from "./CompletedFAChecklistsMap";
+import Button from "@mui/material/Button";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import Stack from "@mui/material/Stack";
 
 const FAMain = ({ completedFA }) => {
   const navigate = useNavigate();
@@ -13,8 +17,25 @@ const FAMain = ({ completedFA }) => {
   return (
     <div>
       <h2>Fall Arrest Checklist</h2>
-      <button onClick={newFA}>Add New Checklist</button>
-      <button onClick={viewComplete}>View Completed Checklists</button>
+      <Stack direction="row" spacing={2}>
+        <Button
+          onClick={newFA}
+          variant="outlined"
+          startIcon={<AddCircleOutlineIcon />}
+        >
+          New Fall Arrest Checklist
+        </Button>
+        <br />
+        <Button
+          onClick={viewComplete}
+          variant="outlined"
+          startIcon={<CheckCircleOutlineIcon />}
+        >
+          View Completed Checklists
+        </Button>
+      </Stack>
+      {/* <button onClick={newFA}>Add New Checklist</button>
+      <button onClick={viewComplete}>View Completed Checklists</button> */}
       <hr />
       <Outlet />
       {/* <h2>Completed Checklists</h2>
